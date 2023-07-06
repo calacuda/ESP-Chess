6d7ba4ecc86b062fd9c177f2a41fcb762661093c
Eoghan West <eowest@gmail.com>
2023-07-06 17:57:20 -0400

    made a lib for the api

--- src/bin/control-node.rs
+++ src/bin/control-node.rs
@@ -1,9 +1,10 @@
-use anyhow;
+use anyhow::{self, bail};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_hal::{i2c, peripherals};
+use esp_idf_hal::{i2c, peripherals, peripheral::Peripheral};
 use log::*;
+use distributed_esp_api::I2C_SPEED;
 
-// pub static I2C_ADR: &str = "";
+pub static I2C_ADDRESS: u8 = 0;
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -12,11 +13,35 @@ fn main() -> anyhow::Result<()> {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("taking peripherals...");
-    let peripherals = peripherals::Peripherals::take()?;
-    info!("peripherals acquired...");
+    let i2c = void_setup()?;
 
     loop {
-        
+        if let Err(why) = void_loop(&i2c) {
+            error!("a loop iteration failed because: {why}");
+        }
     }
+    // }
+}
+
+fn void_setup() -> anyhow::Result<i2c::I2cDriver> {
+    info!("taking peripherals...");
+    let peripherals = match peripherals::Peripherals::take() {
+        Some(periph) => periph,
+        None => bail!("Peripheral could not be taken"),
+    };
+    info!("peripherals acquired...");
+
+    let i2c = i2c::I2cDriver::new(
+        peripherals.i2c0,
+        peripherals.pins.gpio4,
+        peripherals.pins.gpio5,
+        &i2c::config::Config::new().baudrate(I2C_SPEED.into()),
+    )?;  // raw I2C thing
+
+    Ok(i2c)
+}
+
+fn void_loop(i2c: &i2c::I2cDriver) -> anyhow::Result<()> {
+    // TODO: write loop stuff
+    Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+
+pub const I2C_SPEED: usize = 800;  // 400
+
+pub trait I2cCommunication<T> {
+    /// this function will turn the struct into sendable data that can be sent over I2C
+    fn encode(&self) -> anyhow::Result<Arc<[u8]>>;
+    
+    /// this function is the inverse func of encode. it takes the I2C data and converts it back into a rust struct 
+    fn decode_from(&self) -> anyhow::Result<T>;
+}
+
+// pub trait DistributedApiCall<T: I2cCommunication = Self>: I2cCommunication {
+
+// }
+
+// pub trait DistributedApiRes<T: I2cCommunication = Self>: I2cCommunication {
+
+// }
